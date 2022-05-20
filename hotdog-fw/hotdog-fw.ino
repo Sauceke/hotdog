b7f99fb328699e8638090e503013fbb21b283d53
Sauceke <76826783+Sauceke@users.noreply.github.com>
2022-05-21 01:01:14 +0300

    BLE broadcasting, convert output to T-code

--- hotdog-fw/hotdog-fw.ino
+++ hotdog-fw/hotdog-fw.ino
@@ -1,14 +1,16 @@
-// random base64 string
-const String protocolId = "HQNDmlUP";
-const String depthKey = "lvl";
-const char separator = ' ';
+#include <SPI.h>
+#include <RF24.h>
+#include <BTLE.h>
+#include <avrcontext_arduino.h>
+
+#define STACK_SIZE 256
+
+static uint8_t sensorStack[STACK_SIZE], broadcastStack[STACK_SIZE];
+static avr_coro_t sensorState, broadcastState;
 
 // sensors: A0 to A4
 // A0 is the topmost sensor
 const int sensors = 5;
-
-// number of outputs per second
-const int sampleRate = 30;
 
 // how much the sensor reading has to increase to consider it a shadow
 const float threshold = 1.2;
@@ -16,12 +18,17 @@ const float threshold = 1.2;
 // the sensor readings at calibration
 int baseValues[sensors];
 
+// the current depth level (100: fully inserted)
+int level = 0;
+
+RF24 radio(9, 10, 1000000);
+BTLE btle(&radio);
+
 int readSensor(int pin) {
   pinMode(pin, INPUT_PULLUP);
-  delay(1000 / (sampleRate * sensors * 2));
+  delay(1);
   int value = analogRead(pin);
   pinMode(pin, INPUT);
-  delay(1000 / (sampleRate * sensors * 2));
   return value;
 }
 
@@ -31,6 +38,7 @@ void calibrate() {
   for (int i = 0; i < sensors; i++) {
     for (int j = 0; j < rounds; j++) {
       baseValues[i] += readSensor(A0 + i);
+      delay(10);
     }
     baseValues[i] /= rounds;
   }
@@ -38,23 +46,49 @@ void calibrate() {
 
 void setup() {
   Serial.begin(9600);
+  delay(1000);
+  btle.begin("hotdog");
+  radio.setPALevel(RF24_PA_LOW);
   calibrate();
+  avr_coro_init(&sensorState, (void*)sensorStack, STACK_SIZE,
+      (avr_coro_func_t)sensorLoop);
+  avr_coro_init(&broadcastState, (void*)broadcastStack, STACK_SIZE,
+      (avr_coro_func_t)broadcastLoop);
 }
 
 void loop() {
-  float lvl = 0;
-  for (int i = 0; i < sensors; i++) {
-    int value = readSensor(A0 + i);
-    // stop at the first sensor, from top, that detects a shadow
-    if (value > baseValues[i] * threshold) {
-      lvl += sensors - i;
-      delay((sensors - i - 1) * 1000 / (sampleRate * sensors));
-      break;
+  avr_coro_resume(&sensorState, (void *)&level);
+  avr_coro_resume(&broadcastState, (void *)&level);
+}
+
+static void* sensorLoop(avr_coro_t *self, size_t *data) {
+  while (true) {
+    float lvl = 0;
+    for (int i = 0; i < sensors; i++) {
+      avr_coro_yield(self, (void*)data);
+      int value = readSensor(A0 + i);
+      // stop at the first sensor, from top, that detects a shadow
+      if (value > baseValues[i] * threshold) {
+        lvl += sensors - i;
+        break;
+      }
+      lvl = (((float)value) / baseValues[i] - 1) / (threshold - 1);
     }
-    lvl = (((float)value) / baseValues[i] - 1) / (threshold - 1);
+    lvl /= sensors;
+    lvl = max(min(lvl, 1), 0);
+    *data = int(lvl * 100);
   }
-  lvl /= sensors;
-  lvl = max(min(lvl, 1), 0);
-  Serial.println(protocolId + separator + depthKey + separator + String(lvl, 2));
-  Serial.flush();
+}
+
+static void* broadcastLoop(avr_coro_t *self, size_t *data) {
+  while (true) {
+    size_t lvl = *data;
+    char message[10];
+    sprintf(message, "L0%d", 100 - lvl);
+    btle.advertise(0x16, &message, strlen(message));
+    btle.hopChannel();
+    Serial.println(message);
+    delay(1);
+    avr_coro_yield(self, (void*)data);
+  }
 }
